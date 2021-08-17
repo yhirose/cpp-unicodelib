5b8ddc470c6f2f57990f182238fbcbc77f6bcf78
yhirose <yuji.hirose.bug@gmail.com>
2021-08-17 18:44:54 -0400

    Changed to use Catch2 v3

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,8 @@
-﻿#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+﻿#include <catch2/catch_test_macros.hpp>
 
 #include <unicodelib.h>
 #include <unicodelib_encodings.h>
+#include <fstream>
 #include <sstream>
 
 using namespace std;
--- test/test2.cpp
+++ test/test2.cpp
@@ -1,5 +1,5 @@
 ﻿// This file is to check duplicate symbols
-#include "catch.hpp"
+#include <catch2/catch_test_macros.hpp>
 #include <unicodelib.h>
 
 TEST_CASE("Duplicate Check", "[duplicate]") {
